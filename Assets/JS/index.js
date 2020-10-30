440a39771407735324781a07c0c8672ac615abd3
ozzymand <ozzymand@protonmail.com>
2020-10-30 10:19:08 +0200

    Pretty urls

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -4,7 +4,7 @@ const secretCode = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeft
 window.addEventListener('keyup', (e) => {
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-    if(pressed.join('').includes(secretCode)) {
+    if (pressed.join('').includes(secretCode)) {
         console.log('Haxxor Mode Activated');
         cornify_add()
     }
